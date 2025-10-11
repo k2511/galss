d8967160470aa26e42cc1e5ed72bd4d3ca91eca9
k2511 <bhongale.k2511@gmail.com>
2025-10-11 17:24:30 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,11 @@
-// import React from "react";
-// import { Provider } from "react-redux";
-// import { store } from "./app/store";
-// import Header from "./components/Header";
-// import HeroSection from "./components/HeroSection";
-
-// import "./index.css";
-// import TopBar from "./components/TopBar";
-// import EyewearGallery from "./components/EyewearGallery";
-// import CategorySection from "./components/CategorySection";
-// import VirtualTryOnSection from "./components/VirtualTryOnSection";
-// import PopularBrandsSection from "./components/PopularBrandsSection";
-// import SummerPicksSection from "./components/SummerPicksSection";
-// import PerfectEyewearSection from "./components/PerfectEyewearSection";
-// import InsuranceSection from "./components/InsuranceSection";
-// import FramingYourWorldSection from "./components/FramingYourWorldSection";
-
-// import Footer from "./components/Footer";
-// import GlassSection from "./components/GlassSection";
-// import CollectionCards from "./components/CollectionCards";
-// import TrustpilotSection from "./components/TrustpilotSection";
-// import FeaturedAndFilters from "./components/FeaturedAndFilters";
-// import GuaranteeSection from "./components/GuaranteeSection ";
-
-// function App() {
-//   return (
-//     <Provider store={store}>
-//       <div className="min-h-screen bg-white">
-//         <TopBar />
-//         <Header />
-//         <main>
-//           <HeroSection />
-//           <EyewearGallery />
-//           <CategorySection />
-
-//           <VirtualTryOnSection />
-//           <PopularBrandsSection />
-//           <SummerPicksSection />
-//           <PerfectEyewearSection />
-//           <InsuranceSection />
-//           <FramingYourWorldSection />
-//           <GlassSection />
-//           <CollectionCards />
-//           <TrustpilotSection />
-//           <FeaturedAndFilters />
-//           <GuaranteeSection />
-//         </main>
-//         <Footer />
-//       </div>
-//     </Provider>
-//   );
-// }
-
-// export default App;
-
-
-
 import React from "react";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
+import Header from "./components/Header";
+import HeroSection from "./components/HeroSection";
 
 import "./index.css";
-
-// 🧱 Layout Components
 import TopBar from "./components/TopBar";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-
-// 🏠 Home Page Sections
-import HeroSection from "./components/HeroSection";
 import EyewearGallery from "./components/EyewearGallery";
 import CategorySection from "./components/CategorySection";
 import VirtualTryOnSection from "./components/VirtualTryOnSection";
@@ -76,27 +14,25 @@ import SummerPicksSection from "./components/SummerPicksSection";
 import PerfectEyewearSection from "./components/PerfectEyewearSection";
 import InsuranceSection from "./components/InsuranceSection";
 import FramingYourWorldSection from "./components/FramingYourWorldSection";
+
+import Footer from "./components/Footer";
 import GlassSection from "./components/GlassSection";
 import CollectionCards from "./components/CollectionCards";
 import TrustpilotSection from "./components/TrustpilotSection";
 import FeaturedAndFilters from "./components/FeaturedAndFilters";
-import GuaranteeSection from "./components/GuaranteeSection"; 
+import GuaranteeSection from "./components/GuaranteeSection ";
 
 function App() {
   return (
     <Provider store={store}>
-      <div className="min-h-screen bg-white flex flex-col">
-        {/* Top navigation bar */}
+      <div className="min-h-screen bg-white">
         <TopBar />
-
-        {/* Header with dropdown nav, logo, etc. */}
         <Header />
-
-        {/* Main content */}
-        <main className="flex-grow">
+        <main>
           <HeroSection />
           <EyewearGallery />
           <CategorySection />
+
           <VirtualTryOnSection />
           <PopularBrandsSection />
           <SummerPicksSection />
@@ -109,8 +45,6 @@ function App() {
           <FeaturedAndFilters />
           <GuaranteeSection />
         </main>
-
-        {/* Footer */}
         <Footer />
       </div>
     </Provider>
@@ -118,3 +52,6 @@ function App() {
 }
 
 export default App;
+
+
+
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import App from "./App";
-import store from "./app/store";
+import {store} from "./app/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>

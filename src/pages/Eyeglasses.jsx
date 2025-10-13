31cafb0c8e0766d3f5c38487b80ac15f37a475af
Akshay Patil <askhaypatil880537@outlook.com>
2025-10-13 13:20:31 +0530

    chagen

--- src/pages/Eyeglasses.jsx
+++ src/pages/Eyeglasses.jsx
@@ -1,10 +1,13 @@
+import React from 'react'
+
 const Eyeglasses = () => {
   return (
     <div className="p-8 text-center">
       <h1 className="text-3xl font-semibold text-blue-600">Eyeglasses Page</h1>
       <p className="mt-4 text-gray-600">Welcome to the Eyeglasses collection.</p>
     </div>
-  );
-};
+  )
+}
 
-export default Eyeglasses;
+export default Eyeglasses
+
